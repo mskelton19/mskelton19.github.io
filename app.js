1534a119f87c48b458ea9f887aac2ac4fbc201f0
Mike Skelton <mskelton19@Mikes-MacBook-Pro.local>
2017-07-18 13:16:55 -0400

    Created game logic and restart button

--- app.js
+++ app.js
@@ -1,25 +1,69 @@
 $ (() => {
 
+let hobbiton = 0;
+let theDowns = 0;
+let theForest = 0;
 
-const applyRandNum = (square) => {
+const applyRandNum = () => {
   const randNum = Math.floor(Math.random() * 3) + 1;
   // console.log(randNum);
 
-  if(randNum === 1) {
+  if(randNum === 1 && hobbiton > 0) {
     $('.hobbiton').css('background', 'red');
-    console.log("The Eye is on Hobbiton! You've been caught by the Nazgul")
+    lose();
+  } else if (randNum === 1){
+    $('.hobbiton').css('background', 'red');
+    advance();
+  } else if (randNum === 2 && theDowns > 0){
+    $('.theDowns').css('background', 'red');
+    lose();
   } else if (randNum === 2) {
     $('.theDowns').css('background', 'red');
-    console.log("You've been caught in the Barrow Downs");
+    advance();
+  } else if (randNum === 3 && theForest > 0){
+    $('.theForest').css('background', 'red');
+    lose();
   } else {
     $('.theForest').css('background', 'red');
-    console.log('Sauron has spied you in The Old Forest')
+    advance();
   }
 }
-applyRandNum();
+
+const lose = () => {
+  alert('You have been caught by the Nazgul. Sauron has regained the one ring');
+}
+
+const advance = () => {
+  alert("You have avoided Sauron's gaze. The journey continues. Keep it secret, keep it safe")
+}
 
 
+$('.hobbiton').on('click', (e) => {
+  $('.hobbiton').text('You have arrived in Hobbiton');
+  hobbiton++
+  applyRandNum();
+})
 
+$('.theDowns').on('click', (e) => {
+  $('.hobbiton').text('You are in the Barrow Downs');
+  theDowns++
+  applyRandNum();
+})
+
+$('.theForest').on('click', (e) => {
+  $('.hobbiton').text('You are in The Old Forest');
+  theForest++
+  applyRandNum();
+})
+
+$('#restart').on('click', (e) => {
+  theForest = 0;
+  theDowns = 0;
+  hobbiton = 0;
+  $('.hobbiton').css('background', 'green')
+  $('.theDowns').css('background', 'gray')
+  $('.theForest').css('background', 'darkgreen')
+})
 
 
 })
