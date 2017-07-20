af38216beb2132279319d3f3f200ff946c3390fd
Mike Skelton <mskelton19@Mikes-MacBook-Pro.local>
2017-07-20 11:13:19 -0400

    Updated round 1

--- app.js
+++ app.js
@@ -16,10 +16,16 @@ let doom = 0;
 let minas = 0;
 let blackGate = 0;
 
+$('#theShire').hide();
+$('#eriador').hide();
+$('#midEarth').hide();
+$('#mordor').hide();
+
+
 // Round 1
 const round1 = () => {
   const randNum1 = Math.floor(Math.random() * 3) + 1;
-  // console.log(randNum);
+  console.log(randNum1);
 
   if(randNum1 === 1 && hobbiton > 0) {
     $('.hobbiton').css('background-position', 'center center');
@@ -29,7 +35,6 @@ const round1 = () => {
   } else if (randNum1 === 1){
     $('.hobbiton').css('background-position', 'center center');
     $('.hobbiton').css('background-image', 'url(https://ichef.bbci.co.uk/news/660/media/images/79630000/jpg/_79630637_alamydt7hcy.jpg)');
-    lose();
     $('.hobbiton').css('color', 'white');
     advance();
   } else if (randNum1 === 2 && brandywine > 0){
@@ -41,10 +46,12 @@ const round1 = () => {
     $('.brandywine').css('color', 'white');
     advance();
   } else if (randNum1 === 3 && theForest > 0){
-    $('.theForest').css('background', 'red');
+    $('.theForest').css('background-image', 'url(https://ichef.bbci.co.uk/news/660/media/images/79630000/jpg/_79630637_alamydt7hcy.jpg)');
+    $('.theForest').css('color', 'white');
     lose();
   } else {
-    $('.theForest').css('background', 'red');
+    $('.theForest').css('background-image', 'url(https://ichef.bbci.co.uk/news/660/media/images/79630000/jpg/_79630637_alamydt7hcy.jpg)');
+    $('.theForest').css('color', 'white');
     advance();
   }
 }
@@ -52,10 +59,8 @@ const round1 = () => {
 // Round 2
 const round2 = () => {
   const randNum2 = Math.floor(Math.random() * 3) + 1;
-
-  if(firstRound === 0){
-    alert('You must pass through the Shire first.');
-  } else if (randNum2 === 1 && bree > 0){
+  console.log(randNum2);
+    if (randNum2 === 1 && bree > 0){
     $('.bree').css('background', 'red');
     lose();
   } else if (randNum2 === 1){
@@ -70,7 +75,7 @@ const round2 = () => {
   } else if (randNum2 === 3 && moria > 0){
     $('.moria').css('background', 'red');
     lose();
-  } else if (randNum2 === 1){
+  } else if (randNum2 === 3){
     $('.moria').css('background', 'red');
     advance();
 }
@@ -80,9 +85,7 @@ const round2 = () => {
 const round3 = () => {
   const randNum3 = Math.floor(Math.random() * 3) + 1;
 
-  if(secondRound === 0){
-    alert('You must pass through Eriador first.');
-  } else if (randNum3 === 1 && isengard > 0){
+    if (randNum3 === 1 && isengard > 0){
     $('.isengard').css('background', 'red');
     lose();
   } else if (randNum3 === 1){
@@ -97,7 +100,7 @@ const round3 = () => {
   }  else if (randNum3 === 3 && gondor > 0){
     $('.gondor').css('background', 'red');
     lose();
-  } else if (randNum3 === 1){
+  } else if (randNum3 === 3){
     $('.isengard').css('background', 'red');
     advance();
 }
@@ -106,38 +109,45 @@ const round3 = () => {
 const round4 = () => {
   const randNum4 = Math.floor(Math.random() * 3) + 1;
 
-  if(thirdRound === 0){
-    alert('You must pass through Middle Earth first.');
-  } else if (randNum4 === 1 && doom > 0){
+    if (randNum4 === 1 && doom > 0){
     $('.mtDoom').css('background', 'red');
     lose();
   } else if (randNum4 === 1){
     $('.mtDoom').css('background', 'red');
-    advance();
+    win();
   }  else if (randNum4 === 2 && minas > 0){
     $('.morgul').css('background', 'red');
     lose();
   } else if (randNum4 === 2){
     $('.morgul').css('background', 'red');
-    advance();
+    win();
   }  else if (randNum4 === 3 && blackGate > 0){
     $('.gate').css('background', 'red');
     lose();
-  } else if (randNum4 === 1){
+  } else if (randNum4 === 3){
     $('.gate').css('background', 'red');
-    advance();
+    win();
 }
 }
-
 
 const lose = () => {
   alert('You have been caught by the Nazgul. Sauron has regained the one ring');
 }
 
 const advance = () => {
+  if(thirdRound > 0){
+    $('#mordor').show()
+  } else if (secondRound > 0){
+    $('#midEarth').show()
+  } else if (firstRound > 0){
+    $('#eriador').show();
+  }
   alert("You have avoided Sauron's gaze. The journey continues. Keep it secret, keep it safe")
 }
 
+const win = () => {
+  alert('You win')
+}
 
 $('.hobbiton').on('click', (e) => {
   $('.hobbiton').text('Hobbiton*');
@@ -222,14 +232,15 @@ $('.gate').on('click', (e) => {
 
 
 
-// $('#restart').on('click', (e) => {
+$('#start').on('click', (e) => {
+  $('#theShire').show();
 //   theForest = 0;
 //   theDowns = 0;
 //   hobbiton = 0;
 //   $('.hobbiton').css('background', 'green').text('Hobbiton')
 //   $('.theDowns').css('background', 'gray')
 //   $('.theForest').css('background', 'darkgreen')
-// })
+})
 
 
 })
